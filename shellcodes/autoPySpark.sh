c040c17246d9c218be953fcaed0b3bc6cb2fbd84
文軍皓 <58615837+JeffWen0105@users.noreply.github.com>
2020-01-20 18:40:20 +0800

    Update autoPySpark.sh
    Bug修正，並新增關閉Docker伺服器指令(不用在到Virtual box關機)

--- shellcodes/autoPySpark.sh
+++ shellcodes/autoPySpark.sh
@@ -7,6 +7,7 @@ echo " 3 -------查看spark容器的log"
 echo " 4 -------查看正在運作的容器"
 echo " 5 -------顯示虛擬機IP"
 echo " 6 -------刪除spark容器"
+echo " 7 -------關閉Docker伺服器(請先停止所有的container)=>for Windows"
 echo "=======================CHOSE ONE======================="
 
 read -p "Please enter a value: " answer 
@@ -28,10 +29,17 @@ read -p "Please enter a value: " answer
 		5)
 			docker-machine ip ;;
 		6)
+			docker container stop pyspark_exercise
 			docker container rm pyspark_exercise 
 			docker container ls -a;;
-			
-		*) echo "Only choices 0, 1, 2, 3, 4, 5, 6" ;;
+                7) read -p "Are you sure ? (y/N) " ans
+                        if [[ "$ans" == "y" ]]
+                                then
+                                        docker-machine stop  &>/dev/null
+                                        echo "bye bye~~"
+                        fi
+                        ;;
+		*) echo "Only choices 0, 1, 2, 3, 4, 5, 6, 7" ;;
 	esac
 
 exit
